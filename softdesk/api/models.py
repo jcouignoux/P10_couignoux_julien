e0f5f2de92e62b21f34516bbb646fab81043fe07
Julien Couignoux <j.couignoux@gmail.com>
2022-06-07 17:36:44 +0200

    update choicesfields models

--- softdesk/api/models.py
+++ softdesk/api/models.py
@@ -10,7 +10,13 @@ class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=8192)
-    type = models.CharField(max_length=128)
+    TYPE_CHOICES = (
+        ("B", "BackEnd"),
+        ("F", "FrontEnd"),
+        ("I", "iOS"),
+        ("A", "Android")
+    )
+    type = models.CharField(max_length=1, choices=TYPE_CHOICES)
 
     def __str__(self):
         return self.title + str(self.project_id)
@@ -22,10 +28,9 @@ class Contributor(models.Model):
     project_id = models.ForeignKey(
         to=Project, on_delete=models.DO_NOTHING)
     PERM_CHOICES = (
-        ("C", "POST"),
-        ("R", "GET"),
-        ("U", "PUT"),
-        ("D", "DELETE")
+        ("A", "Author"),
+        ("M", "Manager"),
+        ("C", "Creator")
     )
     permission = models.CharField(max_length=1, choices=PERM_CHOICES)
     role = models.CharField(max_length=128)
@@ -38,15 +43,30 @@ class Issue(models.Model):
     issue_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
     desc = models.CharField(max_length=8192)
-    tag = models.CharField(max_length=128)
-    priority = models.CharField(max_length=128)
+    TAG_CHOICES = (
+        ("B", "Bug"),
+        ("I", "Improvement"),
+        ("T", "Task")
+    )
+    tag = models.CharField(max_length=1, choices=TAG_CHOICES)
+    PRIORITY_CHOICES = (
+        ("H", "Hight"),
+        ("M", "Medium"),
+        ("L", "Low")
+    )
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
     project_id = models.ForeignKey(
         to=Project, on_delete=models.DO_NOTHING)
-    status = models.CharField(max_length=128)
+    STATUS_CHOICES = (
+        ("T", "ToDo"),
+        ("I", "InProgress"),
+        ("C", "Closed")
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     author_user_id = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='issue_author')
     assignee_user_id = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='issue_assignee')
+        to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='issue_assignee', default=author_user_id)
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
